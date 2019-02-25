5ce9cb544c07bbf59eb566bab4478dd0236ddfcb
zps <zhaops@soyuan.com.cn>
2019-02-25 14:40:30 +0800

    增加数据库连接

---
+++ src/Core/ConfigHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace Core
+{
+    public class ConfigHelper
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        /// <summary>
+        /// 根据节点读取值
+        /// </summary>
+        /// <param name="nodeName"></param>
+        /// <returns></returns>
+        public static string ReadConfigByName(string name, string nodeName = "appSettings:")
+        {
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            return Configuration[nodeName + name];
+        }
+    }
+}
